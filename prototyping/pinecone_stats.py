229126ffebb5ea6b314fe9699a32eb0b671d38b7
Pratik Kanade <87058253+pratikkanade@users.noreply.github.com>
2025-03-27 19:53:18 -0400

    Delete prototyping/pinecone_stats.py

--- prototyping/pinecone_stats.py
+++
@@ -1,6 +0,0 @@
-from pinecone import Pinecone
-
-pc = Pinecone(api_key="pcsk_2nvmif_P9k6nG7RFt6fvwKZvxK2ywKHSfHWPqvN1r4yw61EyvNj18kFHcZN7CAfu7uiF82")
-index = pc.Index("rag-index")
-stats = index.describe_index_stats()
-print(stats)
